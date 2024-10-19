7c6ea440093fe5764af87de4a8ce79adf6d6d71a
천예준 <yje10007@u.sogang.ac.kr>
2024-10-19 12:45:01 +0700

    test: 특수케이스 테스트케이스 추가
    1. 연산 형식을 잘 지켰는가
    2. 피연산자 제약조건 (양의 정수)
    3. 커스텀 구분자 동작

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -17,10 +17,73 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void 커스텀_미사용() {
+        assertSimpleTest(() -> {
+            run("1,2");
+            assertThat(output()).contains("결과 : 3");
+        });
+    }
+    @Test
+    void 한자리() {
+        assertSimpleTest(() -> {
+            run("3");
+            assertThat(output()).contains("결과 : 3");
+        });
+    }
+    @Test
+    void 이스케이프_문자_커스텀구분자() {
+        assertSimpleTest(() -> {
+            run("//$\\n1$5$5");
+            assertThat(output()).contains("결과 : 11");
+        });
+    }
+
+    @Test
+    void 혼합된_구분자_테스트() {
+        assertSimpleTest(() -> {
+            run("//#\\n1#5,5,3");
+            assertThat(output()).contains("결과 : 11");
+        });
+    }
+
+    @Test
+    void 피연산자_미입력_종료_예외() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("2,3,"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 구분자_중복_예외() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("2,,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+
+    @Test
+    void 음수_예외() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("-1,2,3"))
                 .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 영_예외() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("0,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 숫자가_아닌_피연산자_예외() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("0,c,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
