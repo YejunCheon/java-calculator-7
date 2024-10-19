28a0668f798824d2b1edc48c3cfd5a409159772a
천예준 <yje10007@u.sogang.ac.kr>
2024-10-19 13:16:35 +0700

    fix: 파싱된 숫자 중 0이 존재하는 경우 예외처리

--- src/main/java/calculator/OperationExecutor.java
+++ src/main/java/calculator/OperationExecutor.java
@@ -7,7 +7,7 @@ public class OperationExecutor {
     public long add(long[] operands){
         long sum = 0;
         for (long number : operands) {
-            if (number < 0) {
+            if (number <= 0) {
                 throw new IllegalArgumentException("음수는 입력할 수 없습니다: " + number);
             }
             sum += number;
